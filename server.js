865b1edc26add5239a01d446847aee8d78ddbcbb
Yuk <ksyunk@naver.com>
2022-01-06 15:16:29 +0900

    main

--- server.js
+++ server.js
@@ -1,9 +1,12 @@
 const express = require("express");
 const cors = require("cors");
 const app = express();
-const port = 3000;
+const port = 8080;
 const models = require('./models');
-//업로드 이미지를 관리하는 스토리지 서버로 얼터를 사용학ㅆ다.
+app.use(cors());
+//해당파일을 보여줄때 입력한경로되로 보여주기 위해 세팅
+app.use("/upload",express.static("upload"));
+//업로드 이미지를 관리하는 스토리지 서버로 얼터를 사용하겠다.
 const multer = require('multer');
 //이미지 파일이 오면 어디에 저장할건지
 const upload = multer({
@@ -29,40 +32,45 @@ app.use(cors());
 app.get('/products',async (req,res)=>{
     //get방식 쿼리 데이터 전송
     const queryString = req.query;
-    console.log(queryString.id);
-    console.log(queryString.name);
-    res.send({
-        "products":[
-            {
-                "id":1,
-                "name":"아이방조명",
-                "price":70000,
-                "seller":"그린",
-                "imageUrl":"image/products/product1.jpg"
-            },
-            {
-                "id":2,
-                "name":"아이방조명",
-                "price":70000,
-                "seller":"그린",
-                "imageUrl":"image/products/product2.jpg"
-            },
-            {
-                "id":3,
-                "name":"아이방조명",
-                "price":70000,
-                "seller":"그린",
-                "imageUrl":"image/products/product3.jpg"
-            }
-            
-        ]
-    });
+    // console.log(queryString.id);
+    // console.log(queryString.name);
+    models.Product.findAll({
+        limit:8,
+        order:[
+            ["createdAt","DESC"]
+        ],
+        attrivutes:["id","name","price","seller","createdAt","imageUrl"]
+    })
+    .then((result)=>{
+        res.send({
+            product:result
+        })
+    })
+    .catch((error)=>{
+        console.error(error);
+        res.send('데이터를 가져오지 못했습니다.')
+    })
 })
 //POST방식 응답 지정
 app.post('/products', async(req,res)=>{
     const body = req.body;
-    console.log(body);
-    res.send('상품이 잘 등록되었습니다.');
+    const {name,description,price,seller,imageUrl} = body;
+    models.Product.create({
+        name,
+        description,
+        price,
+        seller,
+        imageUrl:"http://localhost:8080/" + imageUrl
+    }).then((result)=>{
+        console.log("상품 생성 결과 :",result);
+        res.send({
+            result,
+        })
+    })
+    .catch((error)=>{
+        console.error(error);
+        res.send("상품 업로드에 문제가 발생했습니다.")
+    })
 })
 //get방식 경로 파라미터 관리하기
 app.get('/products/:id',async(req,res) => {
@@ -89,10 +97,23 @@ app.get('/products/:id',async(req,res) => {
 app.post('/image',upload.single('image'),(req,res)=>{
     const file = req.file;
     res.send({
-        imageUrl:file.path
+        imageUrl:file.destination +"/"+ file.filename
     })
 })
+
+
 //설정한 app을 실행 시키기
 app.listen(port, ()=>{
     console.log('그린랩프 서버가 돌아가고 있습니다.');
+    models.sequelize
+    //데이터베이스와 동기화(sqlite와 연결) 시키겠다.
+    .sync()
+    .then(()=>{
+        console.log('DB연결성공');
+    })
+    .catch(function(err){
+        console.error(error);
+        console.log('DB연결에러');
+        process.exit();
+    })
 })
